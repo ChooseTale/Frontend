f59952721fe31d8c6428580c6f78755c5ea680f2
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-09-16 02:29:33 +0900

    feat(S0): update mobile wrapper UI

--- apps/game-builder/src/packages/ui/components/NavBar.tsx
+++ apps/game-builder/src/packages/ui/components/NavBar.tsx
@@ -5,8 +5,8 @@ import { NavigationMenu, NavigationMenuItem } from "./ui/NavigationMenu";
 
 export default function NavBar() {
   return (
-    <div className="w-full h-20 shrink-0 bg-black">
-      <NavigationMenu className="w-full h-full max-w-none px-10">
+    <div className="w-full h-16 md:h-20 lg:h-24 shrink-0 bg-black">
+      <NavigationMenu className="w-full h-full max-w-none px-4 md:px-10 lg:px-12">
         <ul className="w-full h-full flex justify-between items-center !mb-0">
           <NavigationMenuItem>
             <Link href="/">
