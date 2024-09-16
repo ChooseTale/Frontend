7439dcbc17479df89d55fcfece7970f202be8be1
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-09-16 18:43:00 +0900

    feat(S0): TypingText 내 미사용 코드 정리, HtmlViewer 삭제

--- apps/game-builder/src/components/common/text/TypingText.tsx
+++ apps/game-builder/src/components/common/text/TypingText.tsx
@@ -5,16 +5,14 @@ export interface TypingTextProps {
   text: string;
   speed?: "slow" | "normal" | "fast";
   initialDelay?: number;
-  hasTypingCursor?: boolean;
   fontSize?: "sm" | "md" | "lg";
+  className?: string;
 }
 
 export default function TypingText({
   text,
   initialDelay = 0,
   speed = "normal",
-  hasTypingCursor = true,
-  fontSize = "md",
 }: TypingTextProps) {
   const typingSpeed = {
     slow: 0.1,
@@ -22,49 +20,24 @@ export default function TypingText({
     fast: 0.02,
   };
   const currentSpeed = typingSpeed[speed];
-  const currentDelay =
-    hasTypingCursor && initialDelay === 0 ? initialDelay + 1 : initialDelay;
+  const currentDelay = initialDelay;
 
   return (
-    <div className="relative">
-      {hasTypingCursor && (
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
+    <>
+      {text.split("").map((char, index) => (
+        <motion.span
+          // eslint-disable-next-line react/no-array-index-key -- Using index as key because content has no unique identifier
+          key={char + index}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{
-            delay: initialDelay,
-            duration: 0.1,
+            delay: currentDelay + index * currentSpeed,
+            duration: 0.001,
           }}
         >
-          <motion.div
-            className="typing-cursor absolute top-6 left-0 w-3 h-[2px] mt-auto mb-1 bg-black"
-            animate={{ opacity: [1, 0, 1] }}
-            transition={{
-              duration: 0.25,
-              repeatDelay: 0.25,
-              repeat: Infinity,
-              repeatType: "loop",
-            }}
-          />
-        </motion.div>
-      )}
-
-      <p className={`text-${fontSize}`}>
-        {text.split("").map((char, index) => (
-          <motion.span
-            // eslint-disable-next-line react/no-array-index-key -- Using index as key because content has no unique identifier
-            key={char + index}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: currentDelay + index * currentSpeed,
-              duration: 0.001,
-            }}
-          >
-            {char}
-          </motion.span>
-        ))}
-      </p>
-    </div>
+          {char}
+        </motion.span>
+      ))}
+    </>
   );
 }
