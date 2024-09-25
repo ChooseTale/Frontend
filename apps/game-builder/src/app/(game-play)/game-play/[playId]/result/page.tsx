e1e63f2c179acec7abbd728b2527a0cbab0cd1b1
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-09-25 19:02:13 +0900

    feat(S2): 엔딩 페이지에 `새로 하기` 버튼 추가

--- apps/game-builder/src/app/(game-play)/game-play/[playId]/result/page.tsx
+++ apps/game-builder/src/app/(game-play)/game-play/[playId]/result/page.tsx
@@ -1,13 +1,21 @@
 import { notFound } from "next/navigation";
 import { getGameResult } from "@/actions/game-play/getGameResult";
 import GamePlayChoosenPages from "@/components/game-play/result/GamePlayChoosenPages";
-import { type GamePlayParams } from "../page";
+import { GamePlaySearchParams, type GamePlayParams } from "../page";
+import GameRestartButton from "@/components/button/GameRestartButton";
 
-export default async function Page({ params }: { params: GamePlayParams }) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: GamePlayParams;
+  searchParams: GamePlaySearchParams;
+}) {
   const { playId } = params;
+  const { gameId } = searchParams;
   const gameInfoResponse = await getGameResult(Number(playId));
 
-  if (isNaN(Number(playId)) || !gameInfoResponse.success) {
+  if (!playId || !gameId || !gameInfoResponse.success) {
     notFound();
   }
 
@@ -20,6 +28,10 @@ export default async function Page({ params }: { params: GamePlayParams }) {
         {gameInfoResponse.result.endingPage.abridgement}
       </h1>
       {lastPage && <GamePlayChoosenPages page={lastPage} />}
+
+      <div className="mt-8 mb-4 flex flex-col gap-2">
+        <GameRestartButton gameId={Number(gameId)} />
+      </div>
     </section>
   );
 }
