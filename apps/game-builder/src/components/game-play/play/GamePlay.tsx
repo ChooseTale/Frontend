080bfd70992fb5f62d41f20703bf604d3912b30c
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-09-27 14:08:33 +0900

    refactor(S2): 게임 play 데이터 패칭 개선

--- apps/game-builder/src/components/game-play/play/GamePlay.tsx
+++ apps/game-builder/src/components/game-play/play/GamePlay.tsx
@@ -10,17 +10,12 @@ interface GamePlayProps {
 }
 
 export default function GamePlay({ playId, gameId, gameIntro }: GamePlayProps) {
-  const pageId = undefined;
+  const pageId = gameIntro.play?.page?.id;
+
   return (
     <section className="relative">
-      {pageId !== undefined ? (
-        <>
-          <PlayInfo gameIntro={gameIntro} />
-          <PlayPage gameId={gameId} playId={playId} pageId={pageId} />
-        </>
-      ) : (
-        <>게임 페이지가 존재하지 않습니다</>
-      )}
+      <PlayInfo gameIntro={gameIntro} />
+      <PlayPage gameId={gameId} playId={playId} pageId={pageId} />
     </section>
   );
 }
