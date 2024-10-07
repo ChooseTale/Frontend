d30bd720ac826b43a9f40c812f3bcecf83c1fcc9
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-07 17:55:16 +0900

    fix(S3): 배경색 UI 오류 수정

--- apps/game-builder/src/app/(game-list)/list/layout.tsx
+++ apps/game-builder/src/app/(game-list)/list/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full w-full bg-white overflow-y-auto  bg-background-dark text-font-dark">
+    <div className="h-full w-full overflow-y-auto bg-background-dark text-font-dark">
       <div className="h-24" />
       {children}
     </div>
