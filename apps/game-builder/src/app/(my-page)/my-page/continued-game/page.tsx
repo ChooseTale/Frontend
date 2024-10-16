6c13b5cdf99ed44ceb5259a0119351b92dac8d40
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-17 06:52:26 +0900

    feat(S6): UI 및 린트 오류 수정

--- apps/game-builder/src/app/(my-page)/my-page/continued-game/page.tsx
+++ apps/game-builder/src/app/(my-page)/my-page/continued-game/page.tsx
@@ -1,8 +1,18 @@
 import { getContinuedGame } from "@/actions/my-page/getContinuedGame";
+import GameListFilters from "@/app/(game-list)/list/_components/game-list-filters/GameListFilters";
+import {
+  formatGameListSearchParams,
+  type GameListSearchParams,
+} from "@/utils/formatGameListSearchParams";
 import TopNav from "../_components/TopNav";
 import ContinuedGameList from "./_components/ContinuedGameList";
 
-export default async function Page() {
+export interface GameListParams {
+  searchParams: GameListSearchParams;
+}
+
+export default async function Page({ searchParams }: GameListParams) {
+  const formattedSearchParams = formatGameListSearchParams(searchParams);
   const continuedGame = await getContinuedGame({
     page: 1,
     limit: 8,
@@ -11,16 +21,14 @@ export default async function Page() {
   });
 
   return (
-    <div className="h-full flex flex-col pb-20">
+    <div className="h-full flex flex-col">
       <TopNav title="진행 중인 게임" hasBackButton page="/my-page" />
-      <ContinuedGameList
-        continuedGame={[
-          ...continuedGame,
-          ...continuedGame,
-          ...continuedGame,
-          ...continuedGame,
-        ]}
-      />
+      <div className="flex justify-between items-center mt-4 mb-5 px-6">
+        <GameListFilters searchParams={formattedSearchParams} />
+      </div>
+      <div className="flex-1 overflow-y-scroll pb-20">
+        <ContinuedGameList continuedGame={continuedGame} />
+      </div>
     </div>
   );
 }
